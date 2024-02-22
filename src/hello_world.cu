1f215cc60641ac3c4aa1bf3507cede2a692cdbbc
jaysinco <jaysinco@163.com>
2024-02-22 09:44:36 +0800

    add cuda debug support

--- src/hello_world.cu
+++ src/hello_world.cu
@@ -2,7 +2,12 @@
 #include <cuda_runtime.h>
 #include <stdio.h>
 
-__global__ void test_print() { printf("Hello World from GPU!\n"); }
+__global__ void test_print()
+{
+    printf("Hello World from GPU1!\n");
+    printf("Hello World from GPU2!\n");
+    printf("Hello World from GPU3!\n");
+}
 
 int hello_world(int argc, char** argv)
 {
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char** argv)
 {
-    // hello_world(argc, argv);
-    check_device(argc, argv);
+    hello_world(argc, argv);
+    // check_device(argc, argv);
     return 0;
 }
