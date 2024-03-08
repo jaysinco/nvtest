d29148a7226f736415eb54e95c014baa164dd848
jaysinco <jaysinco@163.com>
2024-03-08 11:42:29 +0800

    add to hello world

--- src/hello_world.cu
+++ src/hello_world.cu
@@ -1,4 +1,5 @@
 #include "./fwd.cuh"
+#include "./common.cuh"
 #include <cuda_runtime.h>
 #include <stdio.h>
 
@@ -9,9 +10,22 @@ __global__ void test_print()
     printf("Hello World from GPU3!\n");
 }
 
+__global__ void add(int a, int b, int* c) { *c = a + b; }
+
 int hello_world(int argc, char** argv)
 {
-    test_print<<<1, 1>>>();
+    int* d_c;
+    CHECK(cudaMalloc(&d_c, sizeof(int)));
+
+    int a = 2;
+    int b = 9;
+    add<<<1, 1>>>(a, b, d_c);
+
+    int c;
+    CHECK(cudaMemcpy(&c, d_c, sizeof(int), cudaMemcpyDeviceToHost));
+    printf("%d+%d=%d\n", a, b, c);
+    CHECK(cudaFree(d_c));
+
     cudaDeviceSynchronize();
     return 0;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,12 @@
 
 int main(int argc, char** argv)
 {
-    // hello_world(argc, argv);
+    hello_world(argc, argv);
     // check_device(argc, argv);
     // sum_matrix(argc, argv);
     // reduce_integer(argc, argv);
     // nested_hello_world(argc, argv);
     // global_variable(argc, argv);
-    test_cufft(argc, argv);
+    // test_cufft(argc, argv);
     return 0;
 }
